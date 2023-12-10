3841706c8667c80075fcd2e7ecd537f78aa861c3
Remi Zaidan <remi.zaidan@gmail.com>
2023-12-10 23:32:00 +0100

    Setup D10

---
+++ D10/code.py
@@ -0,0 +1,24 @@
+from common import CodeBase
+
+
+class Code(CodeBase):
+
+    def __init__(self):
+        super().__init__('D10')
+
+    def run(self):
+        self.greet()
+
+        for line in self.lines:
+            self.process_line(line)
+
+    def process_line(self, line):
+        print(f'Processing line: {line}')
+
+
+def test():
+    print("Testing...")
+
+
+if __name__ == '__main__':
+    test()
